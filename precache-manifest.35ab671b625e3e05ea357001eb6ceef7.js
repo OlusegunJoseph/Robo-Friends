36fabc99b70d8306a89d9d7b932b6ab323e4192a
Olusegun Joseph Kayode <98201568+OlusegunJoseph@users.noreply.github.com>
2023-01-07 15:35:10 -0500

    Updates

---
+++ precache-manifest.35ab671b625e3e05ea357001eb6ceef7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3048c87294219c133316",
+    "url": "/robo-friends/static/css/main.05c49192.chunk.css"
+  },
+  {
+    "revision": "3048c87294219c133316",
+    "url": "/robo-friends/static/js/main.c01b249b.chunk.js"
+  },
+  {
+    "revision": "74ef7f23abc83f2f7b75",
+    "url": "/robo-friends/static/js/runtime~main.f49acb03.js"
+  },
+  {
+    "revision": "62ec073af585748f4342",
+    "url": "/robo-friends/static/css/2.efa26561.chunk.css"
+  },
+  {
+    "revision": "62ec073af585748f4342",
+    "url": "/robo-friends/static/js/2.d81ff235.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/robo-friends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cc838b181a0f78a54cd31e0375789f2f",
+    "url": "/robo-friends/index.html"
+  }
+];
