faf923f9d82687e45e9c3e3b73e38d54af82a928
Olusegun Joseph Kayode <98201568+OlusegunJoseph@users.noreply.github.com>
2023-01-07 09:37:28 -0500

    updates dependencies and reduced name to h4

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc grow bg-light-green dib br3 pa3 ma2 bw2 shadow-5">
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h2>{name}</h2>
+        <h4>{name}</h4>
         <p>{email}</p>
       </div>
     </div>
